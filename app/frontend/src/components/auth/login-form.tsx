12b55370226ea9978dcb3736ecc8db9069096c99
Seonggil Jeong <sgjeong1108@icloud.com>
2025-12-17 16:44:58 +0900

    feat-front: 회원가입 기능 구현

--- app/frontend/src/components/auth/login-form.tsx
+++ app/frontend/src/components/auth/login-form.tsx
@@ -1,38 +1,43 @@
-import {cn} from "@/lib/utils";
-import {Button} from "@/components/ui/button";
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
-import {Field, FieldDescription, FieldGroup, FieldLabel} from "@/components/ui/field";
-import {Input} from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Field, FieldDescription, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
-export function LoginForm({className, ...props}: React.ComponentProps<"div">) {
+export function LoginForm({ className, ...props }: React.ComponentProps<"div">) {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
-        <CardHeader className='text-center'>
-          <CardTitle className='text-xl'>Welcome back</CardTitle>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Welcome back</CardTitle>
           <CardDescription>Login with your Email</CardDescription>
         </CardHeader>
         <CardContent>
           <form>
             <FieldGroup>
               <Field>
-                <FieldLabel htmlFor='email'>Email</FieldLabel>
-                <Input id='email' type='email' placeholder='m@example.com' required/>
+                <FieldLabel htmlFor="username">ID</FieldLabel>
+                <Input id="username" type="text" placeholder="아이디를 입력해주세요."
+                       value={username}
+                       onChange={(e) => setUsername(e.target.value)} required
+                />
               </Field>
               <Field>
-                <div className='flex items-center'>
-                  <FieldLabel htmlFor='password'>Password</FieldLabel>
-                  <a href='#' className='ml-auto text-sm underline-offset-4 hover:underline'>
-                    Forgot your password?
-                  </a>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
                 </div>
-                <Input id='password' type='password' required/>
+                <Input id="password" type="password"
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)} required />
               </Field>
               <Field>
-                <Button type='submit'>Login</Button>
-                <FieldDescription className='text-center'>
+                <Button type="submit">Login</Button>
+                <FieldDescription className="text-center">
                   Don&apos;t have an account?{" "}
-                  <a href='/signup' className='underline-offset-4 hover:underline'>
+                  <a href="/signup" className="underline-offset-4 hover:underline">
                     Sign up
                   </a>
                 </FieldDescription>
