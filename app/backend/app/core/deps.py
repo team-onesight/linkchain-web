72d9e57e7d0aedebc2de18e6ab5312d3a324f099
Johnberman-J <babo318@gmail.com>
2025-12-26 23:49:23 +0900

    fix: add link_user_map_repository dependency for LinkService

--- app/backend/app/core/deps.py
+++ app/backend/app/core/deps.py
@@ -5,6 +5,7 @@ from db.session import SessionLocal
 from fastapi import Depends, Request
 from repositories.link import LinkRepository
 from repositories.tag import TagRepository
+from repositories.link_user_map import LinkUserMapRepository
 from repositories.user import UserRepository
 from services.link import LinkService
 from services.tag import TagService
@@ -21,8 +22,11 @@ def get_db():
 
 
 def get_di_link_service(db: Annotated[Session, Depends(get_db)]) -> LinkService:
-    repository = LinkRepository(db)  # db 세션을 넘겨 LinkRepository 인스턴스 생성
-    return LinkService(repository)  # LinkService 인스턴스를 반환
+    link_repository = LinkRepository(db)  # db 세션을 넘겨 LinkRepository 인스턴스 생성
+    link_user_map_repository = LinkUserMapRepository(db)  # db 세션을 넘겨 LinkUserMapRepository 인스턴스 생성
+    return LinkService(
+        link_repository=link_repository,
+        link_user_map_repository=link_user_map_repository)  # LinkService 인스턴스를 반환
 
 
 def get_di_tag_service(db: Annotated[Session, Depends(get_db)]) -> TagService:
