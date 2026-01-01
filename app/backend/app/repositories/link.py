9a6513de1afa7745435db4dbe4d431bce0f515f9
Johnberman-J <babo318@gmail.com>
2026-01-01 22:17:28 +0900

    fix: link_id type int -> str, and change repository mehtod name

--- app/backend/app/repositories/link.py
+++ app/backend/app/repositories/link.py
@@ -12,7 +12,7 @@ class LinkRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    def get_link_from_db(self, link_id: int):
+    def get_link_by_link_id(self, link_id: str):
         return self.db.query(Link).filter(Link.link_id == link_id).first()
 
     def get_links_by_user_id(self, user_id: int, cursor: Optional[int], size: int):
