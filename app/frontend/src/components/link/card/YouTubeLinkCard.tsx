8f874c70c7839fcdf3a46d7a0d569811c772373f
Seonggil Jeong <sgjeong1108@icloud.com>
2026-01-02 16:55:58 +0900

    feat: remove LinkCardFactory

--- app/frontend/src/components/link/card/YouTubeLinkCard.tsx
+++ app/frontend/src/components/link/card/YouTubeLinkCard.tsx
@@ -1,35 +1,34 @@
 import * as AspectRatio from "@radix-ui/react-aspect-ratio";
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
-import {PlayCircle} from "lucide-react";
-import type {LinkItem as LinkItemType} from "@/model/link/type";
-import {useNavigate} from "react-router-dom";
-import {LinkCardFooter} from "./CardFooter";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { PlayCircle } from "lucide-react";
+import type { LinkItem as LinkItemType } from "@/model/link/type";
+import { useNavigate } from "react-router-dom";
+import { LinkCardFooter } from "./CardFooter";
 
 interface YouTubeLinkCardProps {
   link: LinkItemType;
 }
 
-export function YouTubeLinkCard({link}: YouTubeLinkCardProps) {
+export function YouTubeLinkCard({ link }: YouTubeLinkCardProps) {
   const navigate = useNavigate();
 
   if (!link) {
     return null;
   }
 
-  const {id, title, description, imageUrl} = link;
+  const { link_id, title, description, imageUrl } = link;
 
   return (
     <Card
-      onClick={() => navigate(`/links/${id}`)}
+      onClick={() => navigate(`/links/${link_id}`)}
       className='flex flex-col h-full overflow-hidden group hover:shadow-lg transition-shadow cursor-pointer'
     >
       <CardHeader className='p-0 relative'>
         <AspectRatio.Root ratio={16 / 9}>
-          <img src={imageUrl} alt={title} className='rounded-t-lg object-cover w-full h-full'/>
+          <img src={imageUrl} alt={title} className='rounded-t-lg object-cover w-full h-full' />
         </AspectRatio.Root>
-        <div
-          className='absolute inset-0 bg-black/20 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity'>
-          <PlayCircle className='h-12 w-12 text-white'/>
+        <div className='absolute inset-0 bg-black/20 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity'>
+          <PlayCircle className='h-12 w-12 text-white' />
         </div>
       </CardHeader>
       <CardContent className='grow p-3'>
@@ -40,7 +39,7 @@ export function YouTubeLinkCard({link}: YouTubeLinkCardProps) {
           {description}
         </CardDescription>
       </CardContent>
-      <LinkCardFooter link={link}/>
+      <LinkCardFooter link={link} />
     </Card>
   );
 }
